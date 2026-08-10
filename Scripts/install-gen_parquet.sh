538dab74a1518e228398fa2ddb4602c9e4d866ee
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-10 12:32:43 -0400

    Update install-gen_parquet.sh

--- Scripts/install-gen_parquet.sh
+++ Scripts/install-gen_parquet.sh
@@ -1,3 +1,13 @@
 python3 -m venv .venv
 source .venv/bin/activate
-pip install pandas numpy elasticsearch faker pyarrow pyarrow.parquet
+pip install pandas numpy elasticsearch faker pyarrow 
+cd /home/elastic/ESQL-DataFederation/Script && ./.venv/bin/python gen_parquet.py
+mc alias set local http://localhost:9000 minioadmin 'datafederation_hooray!'
+mc admin info local
+mc mb local/datasets
+mc cp transactions.parquet local/datasets/transactions/
+mc anonymous set download local/datasets
+mc ls -r local/datasets/
+mc alias list
+mc ls -r local/datasets/
+mc anonymous get local/datasets
